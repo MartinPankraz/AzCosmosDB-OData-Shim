4798a766ce05d6e9ac16b72ed7ec28ffed8fcb9f
MartinPankraz <mapankra@microsoft.com>
2021-05-06 16:36:29 +0200

    project scaffolding

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AzCosmosDB_OData_Shim
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
