98d5092bbd4b8b2a14f7f8ef0476d525b2219bc8
MartinPankraz <mapankra@microsoft.com>
2023-01-03 11:43:10 +0100

    port from net5 to net6, odata + cosmos upgrade

--- publish.bat
+++ publish.bat
@@ -1,6 +1,6 @@
 cd GenericODataWebAPI
 dotnet publish
-cd bin/Debug/net5.0/publish
+cd bin/Debug/net6.0/publish
 PowerShell Compress-Archive -Path * -DestinationPath upload.zip -Force
 call az webapp deployment source config-zip --resource-group %1 --name %2 --src .\upload.zip
 cd ../../../../..
